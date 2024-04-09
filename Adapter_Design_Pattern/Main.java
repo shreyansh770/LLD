3fe4fff8db0ab3e4ce39a39272369aa0b668096d
shreyansh770 <shreyanshthakur1@gmail.com>
2024-04-09 23:31:57 +0530

    some comments

--- Adapter_Design_Pattern/Main.java
+++ Adapter_Design_Pattern/Main.java
@@ -3,11 +3,7 @@ import Adapter.WeightMachineAdapter;
 public class Main {
     
     public static void main(String[] args){
-        WeightMachineAdapter wt = new WeightInKg(new WeightMachineBabies());
+        WeightMachineAdapter wt = new WeightInKg(new WeightMachineBabies()); // telling adapter for which Adaptee it has to act as an Adapter
         System.out.println(wt.getWeightInKg());
     }
-
-
-
-    
 }
